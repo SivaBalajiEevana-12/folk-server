93a9948eda21ff31023710feb1914d4ed6090304
tech-iskconhubli <tech.iskconhubli@gmail.com>
2025-05-18 04:01:56 +0000

    Updated DB connection string

--- src/Config/db.js
+++ src/Config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const Connection = ()=>{
-    return mongoose.connect("mongodb+srv://fixmymill:QMumND5EEonoWU1T@cluster0.damlyra.mongodb.net/")
+    return mongoose.connect("mongodb+srv://techiskconhubli:1Ks6XBK8BrT0b9lq@cluster1.tsna8cn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
 }
 
 module.exports = {Connection}
